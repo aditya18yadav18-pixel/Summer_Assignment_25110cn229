affceba87cc2216ae566b879a3011bec7a6b2c9d
aditya18yadav-pixel <aditya18yadav18@gmail.com>
2026-06-08 21:15:06 +0530

    solved.

---
+++ Day-7/02-fibonacci.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int fib(int n)
+{
+    if(n==1) return 0;
+    if(n==2) return 1;
+    return fib(n-1) + fib(n-2);
+}
+int main()
+{
+    int n;
+    cout<<"enter the number:";
+    cin>>n;
+    cout<<fib(n);
+}
